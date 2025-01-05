f211700b76d599959721433aef4eafb73f4db63e
omlan99 <omlan10616@gmail.com>
2025-01-05 18:51:26 +0600

    added env.local

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -6,14 +6,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCjOVv5SmCv6mOJVdKhXk4ZwPzrMX2UeIQ",
-  authDomain: "visa-navigator-d47f9.firebaseapp.com",
-  projectId: "visa-navigator-d47f9",
-  storageBucket: "visa-navigator-d47f9.firebasestorage.app",
-  messagingSenderId: "995183468589",
-  appId: "1:995183468589:web:1871d70f31544634d798a4"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
-
+ 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
