e26e04e1a64bf9d71196c5f852fd816f4bff36e0
omlan99 <omlan10616@gmail.com>
2025-01-10 14:19:05 +0600

    added design

--- src/Pages/MyVisa.jsx
+++ src/Pages/MyVisa.jsx
@@ -12,7 +12,31 @@ const MyVisa = () => {
     console.log(myVisa)
     return (
         <div>
-            <h3>My visa is coming</h3>
+            <div className="grid grid-cols-4 gap-5 py-5">
+        {myVisa.map((visa) => (
+          <div className="card bg-base-100  shadow-xl border ">
+            <figure className="px-10 pt-10 ">
+              <img
+                src={visa.country_image}
+                alt="country Flag"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body justify-start items-start ">
+
+              <p><span className="font-semibold">Country Name</span> : {visa.country_name}</p>
+              <p><span className="font-semibold">Visa Type</span> : {visa.visa_type}</p>
+              <p><span className="font-semibold">Proceessing Time</span> : {visa.processing_time}</p>
+              <p><span className="font-semibold">Fee</span> : {visa.fee}</p>
+              <p><span className="font-semibold">Validity</span> : {visa.validity}</p>
+              <div className="card-actions mx-auto my-2 flex">
+                <button onClick={() =>handleClick(visa._id)} className="btn btn-primary btn-wide">Updatae</button >
+                <button onClick={() =>handleClick(visa._id)} className="btn btn-primary btn-wide">Delete</button >
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
         </div>
     );
 };
